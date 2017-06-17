38e30cff242d4303e208fd5bbe6e08fd0e78a305
Leonel Antonio Santiago Baez <santiagobaez@msn.com>
2017-06-17 13:02:45 -0400

    Add of server side validation and data annotations

--- NetCoreBasicAccounting/Data/Entities/AccountingParameter.cs
+++ NetCoreBasicAccounting/Data/Entities/AccountingParameter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using NetCoreBasicAccounting.Data.Enums;
@@ -9,10 +10,20 @@ namespace NetCoreBasicAccounting.Data.Entities
     public class AccountingParameter
     {
         public int ID { get; set; }
+        [Required]
+        [Display(Name = "Año")]
         public int ProcessYear { get; set; }
+        [Required]
+        [Display(Name = "Mes")]
         public int Month { get; set; }
+        [Required]
+        [Display(Name = "Mayorizáción Procesada")]
         public MajorizationProcessed MajorizationProcessed { get; set; }
+        [Required]
+        [Display(Name = "RNC")]
         public string Rnc { get; set; }
+        [Required]
+        [Display(Name = "Mes Cierre Fiscal")]
         public int MonthFiscalClose { get; set; }
     }
 }
